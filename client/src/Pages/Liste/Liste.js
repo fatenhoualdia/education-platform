0fd9097e1cd4f58f72e96648f4210dd46b005f6d
faten <fatenkhoualdia@gmail.com>
2023-02-25 12:01:19 +0100

    platformm

---
+++ client/src/Pages/Liste/Liste.js
@@ -0,0 +1,18 @@
+import "./Liste.css";
+import Sidebar from "../../Fixedhash/Sidebar/Sidebar";
+import Navig from "../../Fixedhash/Navigation/Navig";
+import Datatable from "../../Fixedhash/Datatable/Datatable";
+
+const Liste = () => {
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContaineruser">
+        <Navig />
+        <Datatable />
+      </div>
+    </div>
+  );
+};
+
+export default Liste;
